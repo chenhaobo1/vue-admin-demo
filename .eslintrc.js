ce03bdfff6b60519daf26c10d14dfcd041ce72d3
chenhaobo1 <3517730047@qq.com>
2021-08-19 14:15:59 +0800

    构建项目，配置全局样式、路由、UI

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
+  parserOptions: {
+    sourceType: "module",
+    parser: "babel-eslint",
+    ecmaVersion: 6,
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+  },
+};
